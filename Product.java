fc41737c2609e3dac19cae9e9f493efd7e834510
tamzstmarys <2405149@live.stmarys.ac.uk>
2025-05-04 20:07:29 +0100

    Initial commit - working Java project

---
+++ Product.java
@@ -0,0 +1,31 @@
+public class Product {
+    private int id;
+    private String name;
+    private int quantity;
+
+    public Product(int id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String toString() {
+        return id + " | " + name + " | Quantity: " + quantity;
+    }
+}
